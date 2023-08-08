651075fbe2fbd056d4caf1ac80a531e92b48159e
tejb96 <104933640+tejb96@users.noreply.github.com>
2023-08-08 15:39:27 -0600

    bfs

--- myLibrary/src/myLibrary/GraphAlgo/AdjacencyMatrix.java
+++ myLibrary/src/myLibrary/GraphAlgo/AdjacencyMatrix.java
@@ -1,22 +1,41 @@
 package myLibrary.GraphAlgo;
 
 public class AdjacencyMatrix {
-	
+	int[] []adjMat;
+	int numNodes;
 	// Define adjacency matrix
-
+	public AdjacencyMatrix(int numNodes) {
+		this.numNodes=numNodes;
+		adjMat=new int[numNodes][numNodes];
+		for(int i=0;i<numNodes;i++) {
+			for(int j=0;j<numNodes;j++) {
+				adjMat[i][j]=0;
+			}
+		}
+	}
 	// Add new edges
     public void addEdge(int source, int destination, int cost) 
     {
+    	adjMat[source][destination]=cost;
+    	adjMat[destination][source]=cost;	
     }
     
     // return adjacency matrix
     public int[][] getAdjacencyMatrix() 
     {
+    	return adjMat;
     }
 
     // Display adjacency matrix
     public void displayAdjacencyMatrix() 
-    {	        
+    {
+    	for(int i=0;i<numNodes;i++) {
+    		System.out.print("Node "+i+": ");
+			for(int j=0;j<numNodes;j++) {
+				System.out.print(adjMat[i][j]+" ");
+			}
+			System.out.println();
+		}
     }
     
     // Add any other parts needed
--- myLibrary/src/myLibrary/GraphAlgo/BFS.java
+++ myLibrary/src/myLibrary/GraphAlgo/BFS.java
@@ -1,21 +1,58 @@
 package myLibrary.GraphAlgo;
-
+import myLibrary.DataStructures.Linear.Queue;
+import java.util.Arrays;
 public class BFS 
 {
 	
 	// Define BFS 
- 
+	AdjacencyMatrix adjMat;
+	Queue q;
+	Boolean[] visited;
+	int numNodes;
+	int[] visit;
 	// You can use either adjacency matrix or adjacency list to keep track of the graph
 	
+	public BFS(int num_nodes) {
+		// TODO Auto-generated constructor stub
+		numNodes=num_nodes;
+		adjMat=new AdjacencyMatrix(num_nodes);
+		q=new Queue(num_nodes);
+		visited=new Boolean[num_nodes];
+		Arrays.fill(visited, false);
+		visit=new int[num_nodes];
+	}
+
 	// Add edge to graph
     public void addEdge(int v,int w)
     {
+    	adjMat.addEdge(v, w, 1);
     }
  
     // Apply BFS algorithm and display the nodes visited and the visit array values
     // See the sample output to create a proper format
     public void BFS_Algo(int n)
     {
+    	int[][] adjMatrix=adjMat.getAdjacencyMatrix();
+    	int vis=n;
+		q.enqueue(vis);
+		visited[vis]=true;
+		int t=0;
+		while(!q.isEmpty()) {
+			vis=q.dequeue();
+			visit[t]=vis;
+			for(int i=0;i<numNodes;i++) {
+				if(adjMatrix[vis][i]==1&&(!visited[i])) {
+					q.enqueue(i);
+					visited[i]=true;
+					
+				}
+			}
+			t++;
+		}
+		for(int i=0;i<visited.length;i++) {
+			System.out.print(visit[i]+" - ");				
+		}
+		System.out.println("\nVisited nodes: "+ Arrays.toString(visited));
     }
 	
 	// Add any other parts needed
